12203f09553d2dce5066c0e999a8a76a514866d3
govdl <141232612+govdl@users.noreply.github.com>
2023-08-04 16:24:20 +0900

    Update download.php

--- download.php
+++ download.php
@@ -1,30 +1,18 @@
 <?php
 session_start();
 
-$target_Dir="../upload/";
-$file="$filename";
-$down=$target_Dir.$file;
-$filesize=filesize($down);
+$conn = mysqli_connect('localhost', 'root', 'guss1301', 'board');
+$number = $_GET['number'];
+$filename = $_GET["file"];
+$filesize = filesize($filename);
 
-if(file_exists($down)){
-    header("Content-Type:application/octet-stream");
-    header("Content-disposition:attachment;filename=$file");
-    header("Content-Transfer-Encoding:binary");
-    header("Content-Length:".filesize($target_Dir.$file));
-    header("Cache-Control:cache,must-revalidate");
-    header("Pragma:no-cache");
-    header("Expires:0");
-    if(is_file($down)){
-        $fp = fopen($down,"r");
-        while(!feof($fp)){
-            $buf = fread($fp, 8096);
-            $read = strlen($buf);
-            flush();
-        }
-        fclose($fp);
-    }
-}
-else{
-    ?><script>alert("존재하지 않는 파일입니다.")</script><?php
-}
+header("Content-Type: application/octet-stream");
+header("Content-Disposition: attachment; filename='$filename'");
+header("Content-Transfer-encoding: binary");
+header("Content-Length: $filesize");
+
+ob_clean();
+flush();
+readfile($filename);
+
 ?>
