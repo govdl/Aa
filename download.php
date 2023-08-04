c0a6931439bf472e39494b07b8f3ff8349d19dc1
govdl <141232612+govdl@users.noreply.github.com>
2023-08-04 16:26:54 +0900

    Update download.php

--- download.php
+++ download.php
@@ -2,6 +2,10 @@
 session_start();
 
 $conn = mysqli_connect('localhost', 'root', 'guss1301', 'board');
+$query = "SELECT * FROM board WHERE number = '$number'";
+$result = mysqli_query($conn, $query);
+$rows = mysqli_fetch_array($result);
+
 $number = $_GET['number'];
 $filename = $_GET["file"];
 $filesize = filesize($filename);
